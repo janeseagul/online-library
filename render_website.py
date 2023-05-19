a499c9535ae5e8965d8f99eacff040abd8ff2ecc
Marta Chaika <121798160+janeseagul@users.noreply.github.com>
2023-05-19 16:54:57 +0300

    Integrate livereload

--- render_website.py
+++ render_website.py
@@ -1,10 +1,26 @@
 import json
-from http.server import HTTPServer, SimpleHTTPRequestHandler
+from livereload import Server
 
 from jinja2 import Enviroment, FileSystemLoader, select_autoescape
 
 
-def main(books):
+BOOKS_FILENAME = 'books.json'
+
+
+def get_books_from_json(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        books = json.loads(file.read())
+
+    for book in books:
+        book['img_src'] = book['img_src'].replace('\\', '/')
+        book['book_path'] = book['book_path'].replace('\\', '/')
+
+    return books
+
+
+def on_reload():
+    books = get_books_from_json(BOOKS_FILENAME)
+
     env = Enviroment(
         loader=FileSystemLoader('.'),
         autoescape=select_autoescape(['html', 'xml'])
@@ -17,16 +33,9 @@ def main(books):
     with open('index.html', 'w', encoding='utf8') as file:
         file.write(render_page)
 
-    server = HTTPServer(('127.0.0.1', 8000),
-                        SimpleHTTPRequestHandler)
-    server.serve_forever()
-
 
 if __name__=='__main__':
-    with open ('books.json', 'r', encoding='utf-8') as file:
-        books = json.loads(file.read())
-    for book in books:
-        book['img_src'] = book['img_src'].replace('\\', '/')
-        book['book_path'] = book['book_path'].replace('\\', '/')
-
-main(books)
+    on_reload()
+    server = Server()
+    server.watch('template.html', on_reload)
+    server.serve(root='.')
